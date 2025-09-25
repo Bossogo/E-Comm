84822746e2cccce1a084d0f01c49db9183b7637a
Ogooluwa David Ilori <69655851+Bossogo@users.noreply.github.com>
2025-09-25 09:38:14 +0100

    Refactor header structure and add home sections
    Moved Header components to shared directory for better organization. Added HeroSection, BestSellers, and Footer components to the home page layout, preparing for further homepage development.

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,31 @@
-import Header from "@/components/Header";
+import BestSellers from "@/components/home/BestSellers";
+import HeroSection from "@/components/home/HeroSection";
+import Footer from "@/components/shared/Footer";
+import Header from "@/components/shared/Header";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className="bg-[url('/images/hero.png')] min-h-64">
+      <main>
+        {/* Hero Section */}
+        <HeroSection />
+
+        {/* Best Seller */}
+        <BestSellers />
+
+        {/* Adidas Sneaker */}
+
+        {/* Why Us */}
         
+        {/* Latest News */}
+
+        {/* Featured Products */}
+
+        {/* Search Bar */}
       </main>
+      <Footer />
     </>
   );
 }
---
+++ src/components/home/HeroSection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <div className="bg-[url('/images/hero.png')] min-h-64">
+          
+    </div>
+  )
+}
+
+export default HeroSection
